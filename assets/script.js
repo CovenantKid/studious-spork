4e4a282fa94d930e0136f18d4098d3b1b601ac5e
Brad <Bradrockk@gmail.com>
2023-10-09 18:05:15 -0400

    changed generation loop, still incomplete

--- assets/script.js
+++ assets/script.js
@@ -2,23 +2,26 @@
 
 // getting length of the password to be generated
 function generatePassword(){
+var password= []
 var characters=[]
 var length = prompt("How long should the password be?")
-var length = Number(length)
+var length = String(length)
 if (length < 8) alert("below minumum length. (8)") 
 if (length > 128) alert("Above maximum length. (128)")
 // Special characters section
   var SpecChars =confirm("Allow special characters? Ok for yes, Cancel for no.")
   if (SpecChars = true) (SpecChars = ["!","@","#","$","%","^","&","*"])
 // generation section
-    test = SpecChars[Math.floor(Math.random()*SpecChars.length)];
-    console.log (test)
+    for (i = 0; i != length; i++){
+    var UsedCharacters= ["1","%","!","4"]
+    holdover = UsedCharacters[Math.floor(Math.random() * UsedCharacters.length)]
+    password = password + holdover
+    }
+    return password
+  }
+    
 
-
-
-
-
-}
+  
 
 
 
